d1bff7b868692ca3354a6bd54dc49e9e9a9c86b0
Nelany <svetasweetka@gmail.com>
2023-10-18 13:14:58 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  function deletePair(str, bracketsConfig) {
+    for (let i = 0; i < str.length - 1; i++) {
+      for (const pair of bracketsConfig) {
+        if (str[i] === pair[0] && str[i + 1] === pair[1]) {
+          str = str.slice(0, i) + str.slice(i + 2);
+          return deletePair(str, bracketsConfig);
+        }
+      }
+    }
+    return str;
+  }
+  str = deletePair(str, bracketsConfig);
+  return str.length === 0;
+};
